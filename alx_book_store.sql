a31ab9f51ae79e939273309da38803764b883a73
gerald-nyerer <geraldnyerer100@gmail.com>
2025-06-27 10:51:54 +0300

    creating a book store database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     ORDERDETAILID INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantinty DOUBLE,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
